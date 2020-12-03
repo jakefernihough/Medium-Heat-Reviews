de30ddd53c09fb778017c81b5b3a7922e2798928
jakefernihough <jakefernihough@gmail.com>
2020-12-03 15:15:25 +0000

    added edit_reviews functionality

--- app.py
+++ app.py
@@ -100,9 +100,39 @@ def logout():
 
 
 @app.route("/new_reviews")
-def hello():
+def new_reviews():
     reviews = mongo.db.reviews.find()
     return render_template("new_reviews.html", reviews=reviews)
+
+
+@app.route("/add_reviews", methods=["GET", "POST"])
+def add_reviews():
+    if request.method == "POST":
+        review = {
+            "category_name": request.form.get("category_name"),
+            "title": request.form.get("title"),
+            "release_date": request.form.get("release_date"),
+            "created_by": request.form.get("created_by"),
+            "genre": request.form.get("genre"),
+            "length": request.form.get("length"),
+            "review": request.form.get("review"),
+            "rating": request.form.get("rating"),
+            "submitted_by": session["user"]
+        }
+        mongo.db.reviews.insert_one(review)
+        flash("Review Successfully Submitted")
+        return redirect(url_for("latest_reviews"))
+
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template("add_reviews.html", categories=categories)
+
+
+@app.route("/edit_review/<review_id>", methods=["GET", "POST"])
+def edit_review(review_id):
+    review = mongo.db.tasks.find_one({"_id": ObjectId(review_id)})
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template("edit_review.html", review=review,
+        categories=categories)
 
 
 if __name__ == "__main__":
